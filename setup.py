7b3a74c72cef28d670c84376afc7e48eb4668899
Matthew Peddle <matthew.t.peddle@gmail.com>
2014-01-08 07:13:58 -0500

    draft setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'todo-api',
+    version = '0.1',
+    packages = find_packages(),
+    install_requires = [
+        'SQLAlchemy==0.9.1',
+        'Flask==0.10',
+        'Flask-SQLAlchemy==1.0'
+        ],
+    setup_requires=[
+        ],
+    # metadata for upload to PyPI
+    author = 'Matthew Peddle',
+    author_email = 'matthew.t.peddle@gmail.com',
+    description = 'Simple todo API',
+    long_description = '''Simple todo API
+    ''',
+    include_package_data = True,
+    entry_points = '''
+    '''
+)
