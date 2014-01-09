7ab3bd385e9b5f574944b656f18ad00f5ad15ea0
Matthew Peddle <matthew.t.peddle@gmail.com>
2014-01-09 14:38:52 -0500

    Working todo API

--- todoapi/todo.py
+++ todoapi/todo.py
@@ -25,10 +25,9 @@ class TodoApi(MethodView):
         return jsonify({'todos' : [t.api_dict() for t in todo]}) 
 
     def post(self):
-        if not request.json or not 'title' in request.form:
-            abort(400)
-        else:
-            title, text = request.form.title, request.form.get('text','')
+        title, text = request.form.get('title',''), request.form.get('text','')
+        if title == '':
+            return jsonify({'error':'Todos require a title'})
         todo = Todo(title=title,text=text)
         db_session.add(todo)
         db_session.commit()
@@ -43,15 +42,15 @@ class TodoApi(MethodView):
         return jsonify({'result': True})
 
     def put(self, todo_id):
-        if 'title' in request.form and type(request.form['title']) is not unicode:
-            abort(400)
-        if 'text' in request.form and type(request.form['text']) is not unicode:
-            abort(400)
-        title = request.form.title
-        text = request.form.get('text','')
-        todo = Todo.query.filter(Todo.id==todo_id)
+        title, text = request.form.get('title',''), request.form.get('text','')
+        if 'title' =='':
+            return jsonify({'error':'Todos require a title'})
+        todo = Todo.query.filter(Todo.id==todo_id).first()
         if todo:
-            todo.update(title=title,text=text)
+            todo.title = title
+            todo.text = text
+            db_session.add(todo)
+            db_session.commit()
             return jsonify({'result': True})
         else:
             return jsonify({'error':'Todo not found'})
@@ -66,8 +65,9 @@ class Todos(MethodView):
 
 @app.route("/")
 def index():
-    template = env.get_template('todo.html')
-    return template.render(**todos)
+    todos = Todo.query.all()
+    template = jinja_env.get_template('todo.html')
+    return template.render(todos=todos)
 
 app.add_url_rule('/todos', view_func=Todos.as_view('todos_view'), methods=['GET',])
 app.add_url_rule('/todo', view_func=TodoApi.as_view('todo_view_put'), methods=['POST'])
